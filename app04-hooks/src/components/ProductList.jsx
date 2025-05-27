fb97809fd00c1b6400861e12f89547f692b09083
Teeradet <tee2490@hotmail.com>
2025-05-27 10:43:45 +0700

    App04-useCallback

--- app04-hooks/src/components/ProductList.jsx
+++ app04-hooks/src/components/ProductList.jsx
@@ -1,36 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useCallback } from "react";
 import axios from "axios";
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
-  console.log(products);
+  const [url, setUrl] = useState("http://localhost:8000/products");
+  const [counter, setCounter] = useState(0);
+
+  const fetchProducts =useCallback(async () => {
+    const response = await axios.get(url);
+    response.data && setProducts(response.data);
+}, [url]);
+
+useEffect(() => {
+    fetchProducts();
+    console.log("-");
+}, [fetchProducts]);
 
   useEffect(() => {
-    // fetch("http://localhost:8000/products")
-    //   .then((response) => response.json())
-    //   .then((data) => setProducts(data));
+    console.log(counter);
+  }, [counter]);
 
-    axios.get("http://localhost:8000/products").then((response) => {
-      console.log(response);
-      setProducts(response.data);
-    }).catch((error) => {
-      console.error("Error fetching products:", error);
-    });
-  }, []);
+  return (
+    <section>
+      <div className="filter">
+        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+        <button onClick={() => setUrl("http://localhost:8000/products")}>
+          All
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}
+        >
+          In Stock Only
+        </button>
+      </div>
 
-  return     <section>
       {products.map((product) => (
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>
           <p className="name">{product.name}</p>
           <p className="info">
             <span>${product.price}</span>
-            <span className={product.in_stock ? "instock" : "unavailable"}>
-              {product.in_stock ? "In Stock" : "Unavailable"}
+            <span
+              className={product.in_stock == "true" ? "instock" : "unavailable"}
+            >
+              {product.in_stock == "true" ? "In Stock" : "Unavailable"}
             </span>
           </p>
         </div>
       ))}
     </section>
-
+  );
 }
